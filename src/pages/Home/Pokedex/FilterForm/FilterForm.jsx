fa15fc68b01e2ce79cafbcb4ab68e4b523804a12
Alevas004 <aleinfocontent@gmail.com>
2023-06-01 02:16:50 -0500

    solucion problema netlify

--- src/pages/Home/Pokedex/FilterForm/FilterForm.jsx
+++ src/pages/Home/Pokedex/FilterForm/FilterForm.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import "./filterForm.css";
+import "./FilterForm.css";
 import { Form } from "react-router-dom";
 import { getAllTypes } from "../../../../services/getAllTypes";
 
@@ -35,41 +35,38 @@ export const FilterForm = ({ nameInitial, typeInitial }) => {
   }, [typeInitial]);
 
   return (
-    
     <Form className="form">
       <div className="bg">
-      <h2>Searcher</h2>
+        <h2>Searcher</h2>
 
-      <div className="form-input__container">
-        <label htmlFor=""></label>
-        <input
-          value={nameValue}
-          onChange={handleChange}
-          type="text"
-          placeholder="Search Pokemon"
-          name="pokemonName"
-          className="form__input__name"
-        />
-        
+        <div className="form-input__container">
+          <label htmlFor=""></label>
+          <input
+            value={nameValue}
+            onChange={handleChange}
+            type="text"
+            placeholder="Search Pokemon"
+            name="pokemonName"
+            className="form__input__name"
+          />
         </div>
         <div className="form-input__container2">
-        <select
-          name="pokemonType"
-          value={typeValue}
-          onChange={handleTypeChange}
-          className="form__input-type"
-        >
-          <option value="">All</option>
-          {types.map((type) => (
-            <option key={type.id} value={type.id}>
-              {type.name}
-            </option>
-          ))}
-        </select>
-      </div>
-      <button className="btn-search">Search</button>
+          <select
+            name="pokemonType"
+            value={typeValue}
+            onChange={handleTypeChange}
+            className="form__input-type"
+          >
+            <option value="">All</option>
+            {types.map((type) => (
+              <option key={type.id} value={type.id}>
+                {type.name}
+              </option>
+            ))}
+          </select>
+        </div>
+        <button className="btn-search">Search</button>
       </div>
     </Form>
-   
   );
 };
