4930ffb39aa872d899f008a36be1c9249216e0c0
Alevas004 <aleinfocontent@gmail.com>
2023-06-01 02:28:57 -0500

    jgka

--- src/pages/Home/Pokedex/PokemonList/PokemonList.jsx
+++ src/pages/Home/Pokedex/PokemonList/PokemonList.jsx
@@ -1,9 +1,8 @@
 import { useState } from "react";
 import { usePagination } from "../../../../hooks/usePagination";
-import { PagesComponent } from "../pagesComponent/pagesComponent";
+import { PagesComponent } from "../pagesComponent/PagesComponent";
 import PokedexCard from "../pokedexCard/PokedexCard";
-import "./PokemonList.css"
-// import { FilterForm } from "../FilterForm/FilterForm";
+import "./PokemonList.css";
 import { Link } from "react-router-dom";
 
 export const PokemonList = ({ pokemons }) => {
@@ -16,32 +15,34 @@ export const PokemonList = ({ pokemons }) => {
 
   return (
     <section className="section">
-        <PagesComponent
+      <PagesComponent
         totalPages={totalPages}
         onChangePage={changePageTo}
         onNextPage={() => changePageTo(currentPage + 1)}
         onBackPage={() => changePageTo(currentPage - 1)}
-         />
+      />
 
-          <ul className="pokemon-bg">
-           <div className="bg-div">
-            {pokemonSlice.map((pokemon) => (
-              <li key={pokemon.url}>
-
-                <Link style={{color: 'unset', textDecoration: 'unset'}} to={`/pokedex/${pokemon.url.split("/").at(-2)}`}>
+      <ul className="pokemon-bg">
+        <div className="bg-div">
+          {pokemonSlice.map((pokemon) => (
+            <li key={pokemon.url}>
+              <Link
+                style={{ color: "unset", textDecoration: "unset" }}
+                to={`/pokedex/${pokemon.url.split("/").at(-2)}`}
+              >
                 <PokedexCard pokemonId={pokemon.url.split("/").at(-2)} />
-                </Link>
-              </li>
-            ))}
-           </div>
-          </ul>
+              </Link>
+            </li>
+          ))}
+        </div>
+      </ul>
 
-        <PagesComponent
+      <PagesComponent
         totalPages={totalPages}
         onChangePage={changePageTo}
         onNextPage={() => changePageTo(currentPage + 1)}
         onBackPage={() => changePageTo(currentPage - 1)}
-        />
+      />
     </section>
   );
 };
