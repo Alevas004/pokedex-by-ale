f5c8d243261d4a92718db3acc66f4c0ba35e9dd8
Alevas004 <aleinfocontent@gmail.com>
2023-06-08 20:13:58 -0500

    image error solved

--- src/pages/PokemonDetail/PokemonDetail.jsx
+++ src/pages/PokemonDetail/PokemonDetail.jsx
@@ -1,14 +1,55 @@
+// import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom"
+// import { getPokemonById } from "../../services/getPokemonById";
+import Loader from "../../components/loaders/Loader";
 
 
 export const PokemonDetail = () => {
+  // const [pokemonDetail, setPokemonDetail] = useState(null);
+
+  // const allPokemonDetails = ["hp", "attack", "special-attack", "defense", "special-defense", "speed"]
+  // const allStats = pokemonDetail?.stats.filter((stat) =>
+  //   allPokemonDetails.includes(stat.name.toLowerCase())
+  // );
+
+  // useEffect(() => {
+  //   const loadPokemonDetail = async () => {
+  //     const pokemonData = await getPokemonById(pokemonId);
+
+  //     console.log(pokemonData)
+  //     setPokemonDetail(pokemonData);
+  //   };
+  //   loadPokemonDetail();
+  // }, []);
 
     const {pokemonId} = useParams()
+    
 
   return (
     <div>
-        <h1>Pokemon detail</h1>
-        <p>Aquí debe ir información ampliada del pokemon con el ID <mark>{pokemonId}</mark></p>
+
+      {/* {!pokemonId && <Loader/>}
+      {pokemonId && (
+        <>
+        <div>
+          <h1>{pokemonId.name} detail</h1>
+        </div>
+        </>
+        
+      )} */}
+        
+       
+             
+              {/* <ul className="pokemon-card__list-stats">
+                {allStats.map((stat) => (
+                  <li key={stat.name} className="pokemon-card__item">
+                    <div className="div-stats">
+                      <em>{stat.name.toUpperCase()}:</em>
+                      <span> {stat.value}</span>
+                    </div>
+                  </li>
+                ))}
+              </ul> */}
     </div>
   )
 }
